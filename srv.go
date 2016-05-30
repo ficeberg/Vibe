e67f931144800795f5acb3798eb6b01ff4064bea
Festum Qin <Festum@G.PL>
2016-05-30 23:44:46 +0800

    Update JWTWithConfig

--- srv.go
+++ srv.go
@@ -26,7 +26,7 @@ func main() {
 	e.POST("/register", handler.Register)
 
 	r := e.Group("/account")
-	r.Use(middleware.JWTConfig(handler.JWTCheck()))
+	r.Use(middleware.JWTWithConfig(handler.JWTCheck()))
 	r.GET("", handler.TokenResolve)
 	r.POST("/update", handler.Update)
 	r.GET("/info", handler.Get)
